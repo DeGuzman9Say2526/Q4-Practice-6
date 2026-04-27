83ea5518cae58cd7346a8afa913a63151b8dd780
De Guzman-9S-AY2526 <deguzman.etc@obmontessori.edu.ph>
2026-04-28 07:41:35 +0800

    update main

---
+++ main.js
@@ -0,0 +1,5 @@
+function countAnimals(){
+    var animals = ["Whale" , "Monkey" , "Orangutan" , "Gorilla"]; }
+    for (var i = 0; i < animals.length; i++) {
+    window.alert("This zoo contains a " + animals[i] + ".");
+}
